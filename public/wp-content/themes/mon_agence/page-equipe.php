008f0e5768409365f632a7dd874e3633cd85bc14
Léa Marois <leamarois21@gmail.com>
2024-04-02 13:00:13 -0400

    CSS page Équipe

--- public/wp-content/themes/mon_agence/page-equipe.php
+++ public/wp-content/themes/mon_agence/page-equipe.php
@@ -1,20 +1,15 @@
 <?php
 /*Template name: equipe */
 $pageSingle = false;
-$slogan = "";
+$titrePage = get_the_title();
+$slogan = get_the_content();
 get_header(); //Appel de l'inclusion d'entête de page
-echo "page-equipe.php";
 ?>
 
-<main class="page">
+<main class="c_page">
+	<p class="c_h2">Rencontrez nos Wizards!</p>
 
-	<div>
-		<h2><?php the_title() //fonction native WP ?></h2>
-	</div>
-	<p>
-		<?php the_content() ?>
-	</p>
-	<p>Rencontrez nos Wizards!</p>
+	<div class="c_groupeArticle">
 	<?php
 	//Requête et boucle d'affichage des articles avec ACF
 	//À mettre dans les pages utilisant les articles personnalisés et adapter****************
@@ -27,19 +22,24 @@ echo "page-equipe.php";
 	));
 
 	// var_dump($posts);
-
+	
 	if (have_posts()) {
 		//tant qu'il restera des articles
 		foreach ($posts as $post) { ?>
-		<a href="<?php the_permalink(); ?>">
-			<img src="<?php echo get_field("photo_pageEquipe")['sizes']['medium']; ?>" alt="<?php echo $image_info["alt"]; ?>">
-			<p><?php echo get_field("nom_membre") ?></p>
-			<p><?php echo get_field("fonction_membre") ?></p>
-			<hr>
-			<p><?php echo get_field("manda") ?></p>
-	
-		</a>
+		<div class="c_article">
+			<a href="<?php the_permalink(); ?>">
+				<img class="c_articleHat" src="<?php echo get_template_directory_uri()?>/liaisons/images/WizardHat.svg" alt>
+				<img class="c_articleImg" src="<?php echo get_field("photo_pageEquipe")['sizes']['medium']; ?>" aria-labelledby="c_articleNom">
+				<div class="c_figcaption">
+					<p id="c_articleNom" class="c_articleNom"><?php echo get_field("nom_membre") ?></p>
+					<p class="c_articleFonction"><?php echo get_field("fonction_membre") ?></p>
+					<hr>
+					<p class="c_articleManda"><?php echo get_field("manda") ?></p>
+				</div>
+			</a>
+		</div>
 	<?php } }?>
+	</div>
 </main>
 <?php get_sidebar() ?>
 <?php get_footer() ?>
