1420eb90af4f42ce6759c2d1933998e7877e85fe
Sarah Al Mahsani <2036514@csfoy.ca>
2024-04-07 17:53:10 -0400

    modifications finale 2.0

--- public/wp-content/themes/mon_agence/functions.php
+++ public/wp-content/themes/mon_agence/functions.php
@@ -148,17 +148,6 @@ add_action('init', 'agence_services_custom_post', 0);
 add_action('init', 'agence_equipe_custom_post', 0);
 
 
-//Ajout de la librairie Font Awesome pour les icons de la navigation mobile.
-/*function wmpudev_enqueue_icon_stylesheet()
-{
-    wp_register_style('fontawesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
-    wp_enqueue_style('fontawesome');
-}
-
-add_action('wp_enqueue_scripts', 'wmpudev_enqueue_icon_stylesheet');
-*/
-
-
 add_filter( 'jpeg_quality', 'my_prefix_regenerate_thumbnail_quality'); function my_prefix_regenerate_thumbnail_quality() {
     return 100; };
 
